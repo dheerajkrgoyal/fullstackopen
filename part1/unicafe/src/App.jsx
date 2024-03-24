73c13f2907060f3e01c64631e444c80797601823
dheerajkrgoyal <dheerajkrgoyal@gmail.com>
2024-03-24 23:17:50 +0530

    anecdotes

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,15 +8,44 @@ const Button = ({handleClick, text}) => {
     )
 }
 
+const StatisticLine = ({text, value}) => {
+    return (
+        <tr>
+            <td>{text}</td> 
+            <td>{value}</td>
+        </tr>
+    )
+}
+
 const Statistics = ({good, neutral, bad}) => {
+    const all = good + neutral + bad
+    const average = (good-bad)/all
+    const postivePercentage = (good/all)*100
+    if(all > 0){
+        return (
+            <div>
+                <h2>Statistics</h2>
+                <table>
+                    <tbody>
+                        <StatisticLine text='Good' value={good} />
+                        <StatisticLine text='Neutral' value={neutral} />
+                        <StatisticLine text='Bad' value={bad} />
+                        <StatisticLine text='All' value={all} />
+                        <StatisticLine text='Average' value={average} />
+                        <StatisticLine text='Positive' value={postivePercentage} />
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    }
     return (
         <div>
             <h2>Statistics</h2>
-            <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
+            <p>No feedback given</p>
         </div>
     )
+    
 }
 
 const App = () => {
