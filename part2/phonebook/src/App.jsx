4379485d15a9906e58377872d5221ccde343253c
dheerajkrgoyal <dheerajkrgoyal@gmail.com>
2024-03-27 14:40:27 +0530

    more changes to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,16 @@ import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import personService from "./services/persons"
+import Notification from "./components/Notification"
+import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+  const [userMessage, setUserMessage] = useState(null)
+  const [isError, setIsError] = useState(false)
 
   const filteredPerson = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
@@ -43,6 +47,11 @@ const App = () => {
           setPersons(persons.concat(createdObj))
           setNewName('')
           setNewNumber('')
+          setIsError(false)
+          setUserMessage(`Added ${newName}`)
+          setTimeout(() => {
+            setUserMessage(null)
+          }, 2000)
         })
     }else{
       if(persons[duplicateIndex].number !== newNumber){
@@ -52,6 +61,18 @@ const App = () => {
             .update(updateObj.id, updateObj)
             .then(updatedObj => {
               setPersons(persons.map(person => person.id !== updatedObj.id? person : updatedObj))
+              setIsError(false)
+              setUserMessage(`${newName} is updated with new number ${newNumber}`)
+              setTimeout(() => {
+                setUserMessage(null)
+              }, 2000)
+            })
+            .catch(error => {
+              setIsError(true)
+              setUserMessage(`Information of ${newName} has already been removed from the server`)
+              setTimeout(() => {
+                setUserMessage(null)
+              }, 2000)
             })
           setNewName('')
           setNewNumber('')
@@ -68,6 +89,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification userMessage={userMessage} isError={isError} />
       <Filter searchTerm={searchTerm} handleSearchTerm={handleSearchTerm}/>
       
       <h3>Add a new</h3>
